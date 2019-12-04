990acfa69a7748f33fbbcf17e3242efcfb3b2f4f
Sabrina Macmanus <smacmanus@teksystems.com>
2019-12-04 09:27:11 -0600

    added vet list and updated controller. closes #14

--- sfg-pet-clinic/sfg-pet-clinic-master/sfg-pet-clinic/pet-clinic-web/src/main/java/com/company/sfgpetclinic/controllers/VetController.java
+++ sfg-pet-clinic/sfg-pet-clinic-master/sfg-pet-clinic/pet-clinic-web/src/main/java/com/company/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,25 @@
 package com.company.sfgpetclinic.controllers;
 
+import com.company.sfgpetclinic.services.VetServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private VetServiceInterface vetServiceInterface;
+
+    @Autowired
+    public VetController(VetServiceInterface vetServiceInterface) {
+        this.vetServiceInterface = vetServiceInterface;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetServiceInterface.findAll());
 
         return "vets/index";
     }
